a615875e2946235b5d8b2f3ce4826158bce59e28
Matthew Shim <umattdub@gmail.com>
2017-02-19 19:31:39 -0800

    initial commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  moviedb_key: 'a91f7463bf796c20c530346aa675bc4d',
+  appId: 'd0805bbd-757f-46ce-897b-c9e23aebdb01',
+  appPassword: 'oCycbiV4H1VMh92ydLhtTOD',
+  LUIS_APP_ID: '2b623708-217e-499e-be06-fb294e2c4fe4',
+  LUIS_API_KEY: 'bc5839cd681049828561cfa531b505cc',
+  Ocp_Apim_Key: 'a35cb572df1c498d99645726769c9696', // this is from Microsoft Text Analytics API
+}
+
+module.exports = config
---
+++ genres.js
@@ -0,0 +1,53 @@
+// Get the supported genres with mdb.genreList()
+const genres = {
+  action: {
+    id: 28,
+  },
+  adventure: {
+    id: 12,
+  },
+  animation: {
+    id: 16,
+  },
+  comedy: {
+    id: 35,
+  },
+  documentary: {
+    id: 99,
+  },
+  drama: {
+    id: 18,
+  },
+  horror: {
+    id: 27,
+  },
+  mystery: {
+    id: 9648,
+  },
+  romance: {
+    id: 10749,
+  },
+  music: {
+    id: 10402,
+  },
+  thriller: {
+    id: 53,
+  },
+  war : {
+    id: 10752,
+  },
+  western : {
+    id : 37,
+  },
+  scifi : {
+    id: 878,
+  }, 
+  crime : {
+    id: 80
+  },
+  '(quit)': {
+    id: 0,
+  },
+};
+
+module.exports = genres;
